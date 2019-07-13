c9737b0c28d000f70fe4dc2077bb5d5ef7db52fa
Talbot1 <1345067502@qq.com>
2019-07-13 10:24:49 +0800

    udate word2vec

--- txt_word2vec_match.py
+++ txt_word2vec_match.py
@@ -3,12 +3,14 @@
 #min_count,0，100取决于数据集大小， size 几十到几百， wokers = 4
 from gensim.models.word2vec import LineSentence, Word2Vec
 import jieba 
+import numpy as np
 
 
 name_list = []
 poem_dict = {}
 temp = []
 temp1 = []
+poem_match_dict = {}
 stopwords = ['，','。',' ']
 
 def train():
@@ -47,16 +49,36 @@ def match():
     for item in temp:
         name_list.append(temp[temp.index(item)][0])
         poem_dict[temp[temp.index(item)][0]] = temp[temp.index(item)][1]
+    
+    max_similarity = 0
+    min_similarity = 100
+    for i in range(len(poem_dict)):
+        word = poem_dict[name_list[i]]
+        # print(word)
+        txt_cut = jieba.cut_for_search(word)
+        txt_cut = [item for item in txt_cut if item in model_w2v.wv.vocab]
+        for key,value in poem_dict.items():
+            value_cut = list(jieba.cut_for_search(value))
+            value_cut = [item for item in value_cut if item in model_w2v.wv.vocab]
+            poem_match_dict[(name_list[i],key)] = (model_w2v.n_similarity(txt_cut,value_cut))
+            if key == name_list[i]:
+                poem_match_dict[(name_list[i],key)] = 0
+            if max_similarity<poem_match_dict[(name_list[i],key)]:max_similarity = poem_match_dict[(name_list[i],key)]
+            if min_similarity>poem_match_dict[(name_list[i],key)]:min_similarity = poem_match_dict[(name_list[i],key)]
+    
+    for key_n, value in poem_match_dict.items():
+        poem_match_dict[key_n] = (value)/(max_similarity+min_similarity)
+        tn = poem_match_dict[key_n]
+        with open("data/input_data.csv","a",encoding='utf-8') as f:
+            key_n = list(key_n)
+            key_n.append(tn)
+            # print(key_n)
+            for ite in key_n:
+                # print(ite)
+                f.write(str(ite).replace('  ','')+" ")
+            f.write('\n')   
 
-    txt_cut = list(jieba.cut_for_search(txt))
-    txt_cut = [item for item in txt_cut if item in model_w2v.wv.vocab]
-    score = []
-    for key,value in poem_dict.items():
-        value_cut = list(jieba.cut_for_search(value))
-        value_cut = [item for item in value_cut if item in model_w2v.wv.vocab]
-        score.append(model_w2v.n_similarity(txt_cut,value_cut))
-    score.sort(key=lambda x: x)
-    print(score)
+    print(poem_match_dict)
 
 def main():
     flag = input("\nTrain or Match: ")
@@ -68,4 +90,7 @@ def main():
         print("\nMatching finish\n")
 
 if __name__ == "__main__":
-    main()
+    # match()
+    simfilename = 'data/input_data.csv'
+    X = np.genfromtxt(simfilename, delimiter='\t', encoding='utf8', dtype=None)
+    print(X)
